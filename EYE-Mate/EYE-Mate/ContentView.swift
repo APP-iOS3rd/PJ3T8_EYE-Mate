0f3a3d5b5a28ccd532a842689bcd3a75ad59a5c1
S_HYEON <zxl3651@naver.com>
2024-01-22 09:24:57 +0900

    🎉 [Init] 프로젝트 초기 생성

---
+++ EYE-Mate/EYE-Mate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EYE-Mate
+//
+//  Created by 이성현 on 2024/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EYE-Mate/EYE-Mate/EYE_MateApp.swift
@@ -0,0 +1,17 @@
+//
+//  EYE_MateApp.swift
+//  EYE-Mate
+//
+//  Created by 이성현 on 2024/01/22.
+//
+
+import SwiftUI
+
+@main
+struct EYE_MateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
