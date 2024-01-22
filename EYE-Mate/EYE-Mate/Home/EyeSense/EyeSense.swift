8e74ff0118e91a3a1aa5b31f534c555bedcb0712
Minyoung Lee <mminy62@gmail.com>
2024-01-22 14:02:13 +0900

    🎉 [Init] 프로젝트 초기 폴더링

---
+++ EYE-Mate/EYE-Mate/Home/EyeSense/EyeSense.swift
@@ -0,0 +1,18 @@
+//
+//  EyeSense.swift
+//  EYE-Mate
+//
+//  Created by 이민영 on 2024/01/22.
+//
+
+import SwiftUI
+
+struct EyeSense: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EyeSense()
+}
---
+++ EYE-Mate/EYE-Mate/Home/VisionTest/VisionTest.swift
@@ -0,0 +1,18 @@
+//
+//  VisionTest.swift
+//  EYE-Mate
+//
+//  Created by 이민영 on 2024/01/22.
+//
+
+import SwiftUI
+
+struct VisionTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VisionTest()
+}
