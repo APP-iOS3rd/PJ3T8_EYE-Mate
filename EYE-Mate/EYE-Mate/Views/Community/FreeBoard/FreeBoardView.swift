ca89c9c5eced1e26f2f1b2a61fe01e0007f29215
TAEJUN <43903354+htj7425@users.noreply.github.com>
2024-01-23 20:14:56 +0900

    👀 [Feat] 게시글 추가 버튼

--- EYE-Mate/EYE-Mate/Views/Community/FreeBoard/FreeBoardView.swift
+++ EYE-Mate/EYE-Mate/Views/Community/FreeBoard/FreeBoardView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct FreeBoardView: View {
+    @State private var recentsPosts: [String] = []
+    @State private var createNewPost: Bool = false
     var body: some View {
-        Text("FreeBoardView")
+        NavigationStack {
+            ReusablePostsView()
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                .overlay(alignment: .bottomTrailing){
+                    Button {
+                        createNewPost.toggle()
+                    } label: {
+                        Circle()
+                            .foregroundStyle(Color.customGreen)
+                            .frame(maxHeight: 60)
+                            .overlay{
+                                Image(systemName: "square.and.pencil")
+                                    .font(.system(size: 26, weight: .bold))
+                                    .foregroundStyle(.white)
+                            }
+                    }
+                }
+                .padding(15)
+        }
     }
 }
 
