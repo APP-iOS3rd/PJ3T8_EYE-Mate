63c766eb67de4c33337e877481c9cf885ab4925d
TAEJUN <43903354+htj7425@users.noreply.github.com>
2024-01-23 14:20:48 +0900

    👀 [Feat] FAQ 화면 검색바 UI

--- EYE-Mate/EYE-Mate/Views/Community/FAQ/FAQView.swift
+++ EYE-Mate/EYE-Mate/Views/Community/FAQ/FAQView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct FAQView: View {
+    @State private var searchText: String = ""
+    @State private var fetchFAQ: [String] = ["1","2","3"]
     var body: some View {
-        Text("FAQView")
+        VStack {
+            HStack{
+                TextField("어떤 질문을 찾으시나요?", text: $searchText)
+                    .frame(width: .infinity, height: 40)
+                    .padding(.vertical, 10)
+                    .overlay(alignment: .trailing){
+                        Image(systemName: "magnifyingglass")
+                            .font(.title)
+                            .foregroundStyle(Color.customGreen)
+                    }
+            }
+            .padding(.horizontal, 15)
+            .overlay{
+                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                    .stroke(Color.customGreen)
+            }
+            
+        }
+        .padding(.horizontal, 20)
     }
 }
 
