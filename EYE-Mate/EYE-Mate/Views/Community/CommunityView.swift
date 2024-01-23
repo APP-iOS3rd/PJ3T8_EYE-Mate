ddb1e43439bad030b2dde00883074a8b2f4d4de1
TAEJUN <43903354+htj7425@users.noreply.github.com>
2024-01-23 13:48:16 +0900

    👀 [Feat] CommunityView 상단 TabBar 구현
    👀 [Feat] CommunityView 상단 TabBar 구현

--- EYE-Mate/EYE-Mate/Views/Community/CommunityView.swift
+++ EYE-Mate/EYE-Mate/Views/Community/CommunityView.swift
@@ -7,12 +7,21 @@
 
 import SwiftUI
 
+enum CommunityTopTapViewItem : String, CaseIterable {
+    case faq = "FAQ"
+    case freeboard = "자유"
+}
+
 struct CommunityView: View {
+    
+    @State private var selectedPicker: CommunityTopTapViewItem = .faq
+    @Namespace private var animation
+    
     var body: some View {
         VStack {
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 0) {
                 Text("EYE-Mate")
-                    //.font(.pretendardBold_22)
+                //.font(.pretendardBold_22)
                     .font(.system(size: 22, weight: .heavy))
                 
                 HStack{
@@ -26,6 +35,41 @@ struct CommunityView: View {
                 }
             }
             .padding(8)
+            
+            communityTopTabView()
+            
+            switch selectedPicker {
+            case .faq:
+                FAQView()
+            case .freeboard:
+                FreeBoardView()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func communityTopTabView() -> some View {
+        HStack {
+            ForEach(CommunityTopTapViewItem.allCases, id: \.self) { item in
+                VStack {
+                    Text(item.rawValue)
+                        .frame(maxWidth: .infinity/2, minHeight: 30)
+                        .font(.pretendardBold_24)
+                        .foregroundStyle(selectedPicker == item ? .black : .gray)
+                    
+                    if selectedPicker == item {
+                        Rectangle()
+                            .foregroundStyle(Color.customGreen)
+                            .frame(height: 3)
+                            .matchedGeometryEffect(id: "item", in: animation)
+                    }
+                }
+                .onTapGesture {
+                    withAnimation {
+                        self.selectedPicker = item
+                    }
+                }
+            }
         }
     }
 }
