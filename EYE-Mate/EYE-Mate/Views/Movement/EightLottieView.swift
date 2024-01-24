fae477c74539e4e76581dad6f862df393751120b
seongjun.oh <tiger8708@gmail.com>
2024-01-24 17:36:11 +0900

    👀 [Feat] 8자 운동 공 색깔 변경 및 운동 완료에 따른 완료 버튼 구현

--- EYE-Mate/EYE-Mate/Views/Movement/EightLottieView.swift
+++ EYE-Mate/EYE-Mate/Views/Movement/EightLottieView.swift
@@ -12,39 +12,64 @@ import Lottie
 struct EightLottieView: View {
     @Environment(\.dismiss) var dismiss
     @State private var movementPercent = 0.0
+    @State private var isEyeExerciseComplete = false
 
     let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    let customGreenValueProvider = ColorValueProvider(LottieColor(r: 82.0 / 255.0, g: 202.0 / 255.0, b: 166.0 / 255.0, a: 1))
+    let keypath = AnimationKeypath("ball Outlines.Group 1.Fill 1.Color")
 
     var body: some View {
         VStack {
             ProgressView(value: movementPercent, total: 100)
-                .padding(.top, 8)
+                .padding(.top, 16)
                 .progressViewStyle(LinearProgressViewStyle(tint: Color.customGreen))
                 .onReceive(timer) { _ in
                     if movementPercent < 100 {
                         movementPercent += 0.5
+                    } else {
+                        isEyeExerciseComplete = true
                     }}
             ZStack {
                 HStack {
                     Spacer()
-                    Text("노란색 점을 따라 눈을 움직여보세요")
-                        .font(.custom("Pretendard-Bold", size: 30))
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
+                    if isEyeExerciseComplete {
+                        Text("눈 운동 완료!")
+                            .font(.custom("Pretendard-Bold", size: 30))
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                    } else {
+                        Text("초록색 점을 따라 눈을 움직여보세요")
+                            .font(.custom("Pretendard-Bold", size: 30))
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                    }
                     Spacer()
                 }
-                HStack {
-                    Spacer()
-                    Button(action: { dismiss() }) {
-                        Image(systemName: "xmark")
-                            .foregroundColor(.white)
-                            .font(.system(size: 24))
-                            .padding(8)
+                if !isEyeExerciseComplete {
+                    HStack {
+                        Spacer()
+                        Button(action: { dismiss() }) {
+                            Image(systemName: "xmark")
+                                .foregroundColor(.white)
+                                .font(.system(size: 24))
+                                .padding(8)
+                        }
                     }
                 }
             }
-            LottieView(animation: .named("eight_movement"))
-                .looping()
+            if isEyeExerciseComplete {
+                Spacer()
+                CustomBtn(title: "완료", background: Color.customGreen, fontStyle: .pretendardSemiBold_22, action: { dismiss() }).frame(height: 88)
+            } else {
+                Spacer()
+                LottieView(animation: .named("eight-movement"))
+                    .configure({ lottieView in
+                        lottieView.contentMode = .scaleAspectFill
+                        lottieView.setValueProvider(customGreenValueProvider, keypath: keypath)
+                    })
+                    .looping()
+                Spacer()
+            }
         }.onAppear{
             guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
             windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: .landscape))
