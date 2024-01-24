4061df0c7bf2e8a86b336706fea35a175f8034e0
S_HYEON <zxl3651@naver.com>
2024-01-24 21:52:08 +0900

    👀 [Feat] 색채 검사 화면 구현

--- EYE-Mate/EYE-Mate/Views/Home/TestViews/ColorTestView.swift
+++ EYE-Mate/EYE-Mate/Views/Home/TestViews/ColorTestView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct ColorTestView: View {
     var body: some View {
-        Text("ColorTestView")
+        //TODO: - 커스텀 네비게이션 바
+        CustomNavigationTitle(title: "색채 검사", userImg: Image(systemName: "person.fill"))
+        //TODO: - 텍스트 뷰
+        TestTextView(str: "간단한 테스트를 통해\n색채 식별도를 확인해보세요!")
+        Spacer()
+        //TODO: - 온보딩 화면
+        VisionTestOnboardingView(image:[Image("Component1"), Image("Component4")])
+        Spacer()
+        //TODO: - 테스트 시작 버튼
+        RoundedRectangle(cornerRadius: 10)
+            .foregroundColor(.customGreen)
+            .frame(maxWidth: .infinity, maxHeight: 40)
+            .overlay{
+                Button( action: {}, label: {
+                    Text("테스트 시작하기")
+                        .font(.pretendardBold_14)
+                        .foregroundColor(.white)
+                })
+            }
+            .padding(.horizontal, 15)
+        Spacer()
+        //TODO: - 경고 문구
+        WaringText()
+        Spacer()
     }
 }
 
