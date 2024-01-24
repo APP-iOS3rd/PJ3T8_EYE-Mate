47c2ce0ff1817173afbe2be758741b543d09d4da
seongjun.oh <tiger8708@gmail.com>
2024-01-24 16:21:34 +0900

    👀 [Feat] 8자 운동 네비게이션 버튼 변경 및 상단 문구 추가

--- EYE-Mate/EYE-Mate/Views/Movement/EightLottieView.swift
+++ EYE-Mate/EYE-Mate/Views/Movement/EightLottieView.swift
@@ -10,18 +10,42 @@ import UIKit
 import Lottie
 
 struct EightLottieView: View {
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
-           VStack {
-               LottieView(animation: .named("eight_movement"))
-                   .looping()
-           }.onAppear{
-               guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
-               windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: .landscape))
-           }.onDisappear{
-               guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
-               windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: .portrait))
-           }
-       }
+        VStack {
+            ZStack {
+            HStack {
+                Spacer()
+                Text("편안하게 정면을 응시해주세요")
+                    .font(.custom("Pretendard-Bold", size: 30))
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                Spacer()
+            }
+            HStack {
+                Spacer()
+                Button(action: { dismiss() }) {
+                    Image(systemName: "xmark")
+                        .foregroundColor(.white)
+                        .font(.system(size: 24))
+                        .padding(8)
+                }
+            }
+            }
+            LottieView(animation: .named("eight_movement"))
+                .looping()
+        }.onAppear{
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
+            windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: .landscape))
+        }.onDisappear{
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
+            windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: .portrait))
+        }.navigationBarBackButtonHidden(true)
+
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+            .background(.black)
+    }
 }
 
 #Preview {
