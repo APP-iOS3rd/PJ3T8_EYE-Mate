2e5382a8922b033cc19f6f71431b65b22b2d7ecb
S_HYEON <zxl3651@naver.com>
2024-01-24 21:30:17 +0900

    👀 [Feat] 시력검사 화면 구현

--- EYE-Mate/EYE-Mate/Views/Home/TestViews/VisionTestView.swift
+++ EYE-Mate/EYE-Mate/Views/Home/TestViews/VisionTestView.swift
@@ -9,9 +9,55 @@ import SwiftUI
 
 struct VisionTestView: View {
     var body: some View {
-        Text("VisionTestView")
+        //TODO: - 커스텀 네비게이션 바
+        CustomNavigationTitle(title: "시력 검사", userImg: Image(systemName: "person.fill"))
+        //TODO: - 텍스트 뷰
+        VisionTestTextView()
+        Spacer()
+        //TODO: - 온보딩 화면
+        VisionTestOnboardingView(image:[Image("Component1"), Image("Component2"), Image("Component3")])
+        Spacer()
+        //TODO: - 테스트 시작 버튼
+        RoundedRectangle(cornerRadius: 10)
+            .foregroundColor(.customGreen)
+            .frame(maxWidth: .infinity, maxHeight: 40)
+            .overlay{
+                Button( action: {}, label: {
+                    Text("테스트 시작하기")
+                        .font(.pretendardBold_14)
+                        .foregroundColor(.white)
+                })
+            }
+            .padding(.horizontal, 15)
+        Spacer()
+        //TODO: - 경고 문구
+        WaringText()
+        Spacer()
     }
 }
+
+//MARK: - 텍스트 뷰
+private struct VisionTestTextView: View {
+    var body: some View {
+        HStack {
+            Text("간단한 테스트를 통해\n나의 시력을 확인해보세요!")
+                .font(.pretendardRegular_24)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+            Spacer()
+        }
+        
+    }
+}
+
+//MARK: - 테스트 시작 버튼
+private struct StartBtn: View {
+    
+    fileprivate var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
 
 #Preview {
     VisionTestView()
