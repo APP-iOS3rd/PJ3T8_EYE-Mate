e436b1a41760cf0f3ce860f5b03a8dbd9040c2d3
seongjun.oh <tiger8708@gmail.com>
2024-01-24 16:34:35 +0900

    👀 [Feat] Progress View 추가

--- EYE-Mate/EYE-Mate/Views/Movement/EightLottieView.swift
+++ EYE-Mate/EYE-Mate/Views/Movement/EightLottieView.swift
@@ -11,27 +11,37 @@ import Lottie
 
 struct EightLottieView: View {
     @Environment(\.dismiss) var dismiss
+    @State private var movementPercent = 0.0
+
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
 
     var body: some View {
         VStack {
+            ProgressView(value: movementPercent, total: 100)
+                .padding(.top, 8)
+                .progressViewStyle(LinearProgressViewStyle(tint: Color.customGreen))
+                .onReceive(timer) { _ in
+                    if movementPercent < 100 {
+                        movementPercent += 0.5
+                    }}
             ZStack {
-            HStack {
-                Spacer()
-                Text("편안하게 정면을 응시해주세요")
-                    .font(.custom("Pretendard-Bold", size: 30))
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-                Spacer()
-            }
-            HStack {
-                Spacer()
-                Button(action: { dismiss() }) {
-                    Image(systemName: "xmark")
+                HStack {
+                    Spacer()
+                    Text("노란색 점을 따라 눈을 움직여보세요")
+                        .font(.custom("Pretendard-Bold", size: 30))
                         .foregroundColor(.white)
-                        .font(.system(size: 24))
-                        .padding(8)
+                        .multilineTextAlignment(.center)
+                    Spacer()
                 }
-            }
+                HStack {
+                    Spacer()
+                    Button(action: { dismiss() }) {
+                        Image(systemName: "xmark")
+                            .foregroundColor(.white)
+                            .font(.system(size: 24))
+                            .padding(8)
+                    }
+                }
             }
             LottieView(animation: .named("eight_movement"))
                 .looping()
