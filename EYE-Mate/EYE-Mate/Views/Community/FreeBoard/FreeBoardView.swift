454c25fc9ea37b8f99c624298a844d6344f462c6
TAEJUN <43903354+htj7425@users.noreply.github.com>
2024-01-25 01:20:22 +0900

    👀 [Feat] 게시물 추가 버튼 그림자 수정

--- EYE-Mate/EYE-Mate/Views/Community/FreeBoard/FreeBoardView.swift
+++ EYE-Mate/EYE-Mate/Views/Community/FreeBoard/FreeBoardView.swift
@@ -9,28 +9,25 @@ import SwiftUI
 
 struct FreeBoardView: View {
     @State private var recentsPosts: [String] = ["안약 과다 사용", "안약 과다 사용2", "안약 과다 사용3", "안약 과다 사용4", "안약 과다 사용5", "안약 과다 사용6", "안약 과다 사용7"]
-    @State private var createNewPost: Bool = false
     var body: some View {
-        NavigationStack {
-            ReusablePostsView(posts: $recentsPosts)
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                .overlay(alignment: .bottomTrailing){
-                    Button {
-                        createNewPost.toggle()
-                    } label: {
-                        Circle()
-                            .foregroundStyle(Color.customGreen)
-                            .frame(maxHeight: 60)
-                            .overlay{
-                                Image(systemName: "square.and.pencil")
-                                    .font(.system(size: 26, weight: .bold))
-                                    .foregroundStyle(.white)
-                            }
-                            .shadow(color: Color(white: 0.0, opacity: 0.25), radius: 0, x: 2, y: 2)
-                    }
-                    .padding()
+        ReusablePostsView(posts: $recentsPosts)
+            .frame(maxWidth: .infinity, alignment: .center)
+        
+            .overlay(alignment: .bottomTrailing){
+                NavigationLink(destination: CreateNewPostView()) {
+                    Circle()
+                        .foregroundStyle(Color.customGreen)
+                        .frame(maxHeight: 60)
+                        .shadow(color: Color(white: 0.0, opacity: 0.25), radius: 0, x: 2, y: 2)
+                        .overlay{
+                            Image(systemName: "square.and.pencil")
+                                .font(.system(size: 26, weight: .bold))
+                                .foregroundStyle(.white)
+                        }
                 }
-        }
+                .padding()
+            }
+        
     }
 }
 
