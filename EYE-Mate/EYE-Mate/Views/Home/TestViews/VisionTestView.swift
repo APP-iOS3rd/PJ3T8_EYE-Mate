ecf7879347e42a45b763be205523201c32b903b6
S_HYEON <zxl3651@naver.com>
2024-01-24 21:34:03 +0900

    👀 [Feat] 검사 화면 텍스트 뷰 컴포넌트로 구현

--- EYE-Mate/EYE-Mate/Views/Home/TestViews/VisionTestView.swift
+++ EYE-Mate/EYE-Mate/Views/Home/TestViews/VisionTestView.swift
@@ -12,7 +12,7 @@ struct VisionTestView: View {
         //TODO: - 커스텀 네비게이션 바
         CustomNavigationTitle(title: "시력 검사", userImg: Image(systemName: "person.fill"))
         //TODO: - 텍스트 뷰
-        VisionTestTextView()
+        TestTextView(str: "간단한 테스트를 통해\n나의 시력을 확인해보세요!")
         Spacer()
         //TODO: - 온보딩 화면
         VisionTestOnboardingView(image:[Image("Component1"), Image("Component2"), Image("Component3")])
@@ -35,29 +35,6 @@ struct VisionTestView: View {
         Spacer()
     }
 }
-
-//MARK: - 텍스트 뷰
-private struct VisionTestTextView: View {
-    var body: some View {
-        HStack {
-            Text("간단한 테스트를 통해\n나의 시력을 확인해보세요!")
-                .font(.pretendardRegular_24)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 10)
-            Spacer()
-        }
-        
-    }
-}
-
-//MARK: - 테스트 시작 버튼
-private struct StartBtn: View {
-    
-    fileprivate var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
-    }
-}
-
 
 #Preview {
     VisionTestView()
