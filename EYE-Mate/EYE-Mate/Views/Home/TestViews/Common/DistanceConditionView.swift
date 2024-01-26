943045c646e8f7842384265c1e7f9a3db41770fc
S_HYEON <zxl3651@naver.com>
2024-01-26 15:45:18 +0900

    👀 [Feat] ViewModel 기능 추가

--- EYE-Mate/EYE-Mate/Views/Home/TestViews/Common/DistanceConditionView.swift
+++ EYE-Mate/EYE-Mate/Views/Home/TestViews/Common/DistanceConditionView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct DistanceConditionView: View {
     @ObservedObject var viewModel = DistanceConditionViewModel()
-    var title: String
+    @State var title: String
     
     init(title: String) {
         self.title = title
@@ -19,46 +19,8 @@ struct DistanceConditionView: View {
         NavigationStack {
             ZStack {
                 DistanceFaceAndDevice(model: viewModel)
-                GeometryReader { g in
-                    Rectangle()
-                        .ignoresSafeArea()
-                        .frame(width: g.size.width, height: g.size.height)
-                        .foregroundColor(.white)
-                }
-                VStack {
-                    //TODO: - Custom 상단바 구현
-                    Spacer()
-                        .frame(maxHeight: 100)
-                    Text("\(title)를 위해서 휴대폰을 사용자와\n40cm ~ 50cm 간격을 유지해주세요!")
-                        .font(.pretendardMedium_20)
-                        .multilineTextAlignment(.center)
-                    Spacer()
-                    HStack{
-                        Spacer()
-                        Text("현재거리 ")
-                            .font(.pretendardRegular_30)
-                        Text("\(viewModel.distance)")
-                            .font(.pretendardRegular_40)
-                            .foregroundColor(viewModel.canStart ? .customGreen : .customRed)
-                        Text("CM")
-                            .font(.pretendardRegular_30)
-                        Spacer()
-                    }
-                    Spacer()
-                    // 만약 거리가 40~50cm 이내라면
-                    //TODO: - 테스트가 가능한지 안가능한지 여부 Text + 여부에 따라 버튼 활성화 및 비활성화
-                    VStack {
-                        Text(viewModel.canStart ? "테스트가 가능합니다." : "테스트 가능한 거리가 되면\n버튼이 활성화됩니다.")
-                            .font(.pretendardMedium_20)
-                            .foregroundColor(viewModel.canStart ? .customGreen : .customRed)
-                            .multilineTextAlignment(.center)
-                            .frame(height: 50)
-                        CustomBtn(title: "테스트 시작하기", background: viewModel.canStart ? .customGreen : .btnGray, fontStyle: .pretendardMedium_18, action: {})
-                            .frame(maxHeight: 75)
-                            .disabled(!viewModel.canStart)
-                    }
-                    
-                }
+                BackgroundView()
+                DistanceView(title: $title)
                 .toolbar {
                     ToolbarItem(placement: .principal) {
                         Text(title)
@@ -76,6 +38,65 @@ struct DistanceConditionView: View {
         }
     }
 }
+
+//MARK: - 설명 Text와 거리 Text View
+private struct DistanceView: View {
+    @ObservedObject var viewModel = DistanceConditionViewModel()
+    @Binding var title: String
+    
+    
+    var body: some View {
+        VStack {
+            Spacer()
+                .frame(maxHeight: 100)
+            
+            Text("\(title)를 위해서 휴대폰을 사용자와\n40cm ~ 50cm 간격을 유지해주세요!")
+                .font(.pretendardMedium_20)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            HStack{
+                Spacer()
+                Text("현재거리 ")
+                    .font(.pretendardRegular_30)
+                Text("\(viewModel.distance)")
+                    .font(.pretendardRegular_40)
+                    .foregroundColor(viewModel.canStart ? .customGreen : .customRed)
+                Text("CM")
+                    .font(.pretendardRegular_30)
+                Spacer()
+            }
+            
+            Spacer()
+            
+            VStack {
+                Text(viewModel.informationText)
+                    .font(.pretendardMedium_20)
+                    .foregroundColor(viewModel.canStart ? .customGreen : .customRed)
+                    .multilineTextAlignment(.center)
+                    .frame(height: 50)
+                CustomBtn(title: "테스트 시작하기", background: viewModel.canStart ? .customGreen : .btnGray, fontStyle: .pretendardMedium_18, action: {})
+                    .frame(maxHeight: 75)
+                    .disabled(!viewModel.canStart)
+            }
+            
+        }
+    }
+}
+
+//MARK: - Background 뷰
+private struct BackgroundView: View {
+    var body: some View {
+        GeometryReader { g in
+            Rectangle()
+                .ignoresSafeArea()
+                .frame(width: g.size.width, height: g.size.height)
+                .foregroundColor(.white)
+        }
+    }
+}
+
 
 #Preview {
     DistanceConditionView(title: "검사")
--- EYE-Mate/EYE-Mate/Views/Home/TestViews/Common/DistanceConditionViewModel.swift
+++ EYE-Mate/EYE-Mate/Views/Home/TestViews/Common/DistanceConditionViewModel.swift
@@ -11,12 +11,12 @@ import Foundation
 class DistanceConditionViewModel: ObservableObject {
     @Published var distance: Int = 0
     
-    init() {
-        
-    }
-    
     var canStart: Bool {
         return distance >= 40 && distance <= 50
+    }
+    
+    var informationText: String {
+        canStart ? "테스트가 가능합니다." : "테스트 가능한 거리가 되면\n버튼이 활성화됩니다."
     }
     
     @MainActor
