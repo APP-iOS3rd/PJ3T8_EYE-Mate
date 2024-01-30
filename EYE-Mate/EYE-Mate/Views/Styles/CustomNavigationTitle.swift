0b92c3c0adeb233e2eec5044b0607999da9f6c8d
S_HYEON <zxl3651@naver.com>
2024-01-31 01:14:17 +0900

    👀 [Feat] CustomNavigationTitle 파일 뒤로가기 버튼 및 로그인 여부에 따라 프로필 이미지 변경 구현

--- EYE-Mate/EYE-Mate/Views/Styles/CustomNavigationTitle.swift
+++ EYE-Mate/EYE-Mate/Views/Styles/CustomNavigationTitle.swift
@@ -8,33 +8,59 @@
 import SwiftUI
 
 struct CustomNavigationTitle: View {
-    var title: String
-    var userImg: Image
+    let title: String
+    let userImg: Image
+    let isDisplayLeftBtn: Bool
+    let leftBtnAction: () -> Void
+    let profileBtnAction: () -> Void
+    @State private var loggedIn = UserDefaults.standard.bool(forKey: "Login")
+    @Environment(\.dismiss) private var dismiss
     
-    init(title: String, userImg: Image = Image(systemName: "person.fill")) {
+    init(title: String, 
+         userImg: Image = Image(systemName: "person.fill"),
+         isDisplayBtn: Bool = true,
+         leftBtnAction: @escaping () -> Void = {},
+         profileBtnAction: @escaping () -> Void = {}
+    ) {
         self.title = title
         self.userImg = userImg
+        self.isDisplayLeftBtn = isDisplayBtn
+        self.leftBtnAction = leftBtnAction
+        self.profileBtnAction = profileBtnAction
     }
     
     var body: some View {
-        NavigationStack {
-            HStack {
-                VStack {
+            HStack(alignment: .lastTextBaseline) {
+                if isDisplayLeftBtn {
+                    Button(action: { dismiss() }, 
+                           label: {
+                        Image("leftArrow")
+                    })
+                }
+                VStack(alignment: .leading) {
                     Text("EYE-Mate")
                         .font(.pretendardSemiBold_22)
                     Text(title)
                         .font(.pretendardBold_32)
                 }
+                
                 Spacer()
                 
-                NavigationLink(destination: ProfileView()) {
-                    userImg
-                        .resizable()
-                        .frame(width: 50, height: 50)
+                if loggedIn {
+                    Button(action: profileBtnAction, label: {
+                        userImg
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                    })
+                } else {
+                    Button(action: profileBtnAction, label: {
+                        Image("defaultprofile")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                    })
                 }
             }
-            .padding([.leading, .trailing], 20)
-        }
+            .padding(.horizontal, 20)
     }
 }
 
