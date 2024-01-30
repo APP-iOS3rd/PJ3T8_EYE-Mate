9deeee5dc8a41088086a584df46fc4914f8e75b0
seongjun.oh <tiger8708@gmail.com>
2024-01-30 16:30:44 +0900

    👀 [Feat] 기록 화면 상단 컴포넌트 구성

--- EYE-Mate/EYE-Mate/Views/Home/Record/RecordView.swift
+++ EYE-Mate/EYE-Mate/Views/Home/Record/RecordView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct RecordView: View {
+    @Environment(\.dismiss) var dismiss
+
+    private func goBack() {
+        dismiss()
+    }
+
     var body: some View {
-        Text("RecordView")
+        VStack(spacing: 0) {
+            HStack {
+                VStack(alignment: .leading, spacing: 12){
+                    Text("EYE-Mate")
+                        .font(.pretendardSemiBold_22)
+                    HStack {
+                        Button {
+                            goBack()
+                        } label: {
+                            Image(systemName: "chevron.left")
+                                .foregroundColor(.black)
+                        }
+                        Text("기록")
+                            .font(.pretendardSemiBold_32)
+                    }
+                }
+                Spacer()
+                Circle()
+                    .foregroundColor(Color.blue)
+                    .frame(width: 50, height: 50)
+            }
+            .frame(height: 112)
+            .padding(.horizontal, 24)
+            HorizontalDivider(color: Color.customGreen, height: 4)
+        }
+        Spacer()
     }
 }
 
