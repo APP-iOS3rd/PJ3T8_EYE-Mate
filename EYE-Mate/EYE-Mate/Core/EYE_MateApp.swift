129ebfdf9c13063e5e716567b6a0a76bd5595118
Minyoung Lee <mminy62@gmail.com>
2024-01-30 16:29:11 +0900

    👀 [Feat] Fireabase Auth 연결

--- EYE-Mate/EYE-Mate/Core/EYE_MateApp.swift
+++ EYE-Mate/EYE-Mate/Core/EYE_MateApp.swift
@@ -7,16 +7,7 @@
 
 import SwiftUI
 import FirebaseCore
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
-}
-
+import FirebaseAuth
 
 @main
 struct EYE_MateApp: App {
@@ -24,12 +15,38 @@ struct EYE_MateApp: App {
     // register app delegate for Firebase setup
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
-//    init() {
-//        FirebaseApp.configure()
-//    }
     var body: some Scene {
         WindowGroup {
             MainView()
         }
+    }
+}
+
+// firebase 연결함수
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+    // 토큰 받아오는 함수
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Auth.auth().setAPNSToken(deviceToken, type: .sandbox)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // 사용자의 데이터 보내기
+        // 우선 nodata로 아무것도 안보냄
+        if Auth.auth().canHandleNotification(userInfo) {
+            completionHandler(.noData)
+        }
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if Auth.auth().canHandle(url){
+            return true
+        }
+        return false
     }
 }
