eee57c6285e9543fd52f05c8e320d548500a8e25
seongjun.oh <tiger8708@gmail.com>
2024-02-01 16:08:52 +0900

    👀 [Feat] 시력 정보 추가 화면 Header UI 구현

---
+++ EYE-Mate/EYE-Mate/Views/Home/Record/AddRecord/AddRecordView.swift
@@ -0,0 +1,21 @@
+//
+//  AddRecordView.swift
+//  EYE-Mate
+//
+//  Created by seongjun on 2/1/24.
+//
+
+import SwiftUI
+
+struct AddRecordView: View {
+    var body: some View {
+        VStack {
+            AddRecordHeader()
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    AddRecordView()
+}
