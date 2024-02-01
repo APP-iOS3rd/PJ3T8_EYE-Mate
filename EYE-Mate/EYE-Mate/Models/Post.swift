0b62bf5a58b5f94b9412f72a35ed0eb7aa03cb5d
TAEJUN <43903354+htj7425@users.noreply.github.com>
2024-02-01 14:41:15 +0900

    ♻️ [Refactor] 게시물 Model Refactoring

--- EYE-Mate/EYE-Mate/Models/Post.swift
+++ EYE-Mate/EYE-Mate/Models/Post.swift
@@ -12,7 +12,7 @@ struct Post: Identifiable, Codable, Equatable, Hashable {
     @DocumentID var id: String?
     var postTitle: String // 게시물 제목
     var postContent: String // 게시물 내용
-    var imageURL: [URL]? // Image 받아올 URL
+    var postImageURLs: [URL]? // Image 받아올 URL
     var imageReferenceID: String = "" // Image 삭제, 수정 할 때 사용
     var publishedDate: Date = Date() // 게시 Date
     var likedIDs: [String] = [] // 좋아요 누른 userID Array
@@ -20,7 +20,7 @@ struct Post: Identifiable, Codable, Equatable, Hashable {
     // MARK: 유저 정보
     var userName: String
     var userUID: String
-//    var userImageURL: URL  // 사용자 ProfileImage
+    var userImageURL: URL  // 사용자 ProfileImage
     
     var comments: [Comment] = []
 }
@@ -30,10 +30,11 @@ struct Comment: Codable, Equatable, Hashable {
     // MARK: 유저정보
     var userName: String
     var userUID: String
-//    var userProfileURL: URL
+    var userImageURL: URL  // 사용자 ProfileImage
+    
     
     var comment: String
-//    var replyComments: [Comment]?
+    var publishedDate: Date = Date() // 게시 Date
     var replyComments: [ReplyComment]?
 }
 
@@ -42,7 +43,8 @@ struct ReplyComment: Codable, Equatable, Hashable {
     // MARK: 유저정보
     var userName: String
     var userUID: String
-//    var userProfileURL: URL
-    
+    var userImageURL: URL  // 사용자 ProfileImage
+
     var comment: String
+    var publishedDate: Date = Date() // 게시 Date
 }
