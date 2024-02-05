bb078b100f40a7463bb608adb0463f93cf57beab
Minyoung Lee <mminy62@gmail.com>
2024-02-05 11:59:52 +0900

    💄 [Design] setting 네비게이션 타이틀뷰 추가

---
+++ EYE-Mate/EYE-Mate/Views/Styles/SettingNavigationTitle.swift
@@ -0,0 +1,51 @@
+//
+//  SettingNavigationTabBar.swift
+//  EYE-Mate
+//
+//  Created by 이민영 on 2024/02/05.
+//
+
+import SwiftUI
+
+enum SettingNavigationBtnType: String {
+    case close = "xmark"
+    case back = "chevron.backward"
+}
+
+struct SettingNavigationTitle: View {
+    let isDisplayTitle: Bool
+    let leftBtnAction: () -> Void
+    let leftBtnType: SettingNavigationBtnType
+    let title: String
+    
+    init(
+        isDisplayTitle: Bool = true,
+        leftBtnAction: @escaping () -> Void = {},
+        leftBtnType: SettingNavigationBtnType = .back,
+        title: String = "닉네임 변경"
+    ) {
+        self.isDisplayTitle = isDisplayTitle
+        self.leftBtnAction = leftBtnAction
+        self.leftBtnType = leftBtnType
+        self.title = title
+    }
+    
+    
+    var body: some View {
+        HStack {
+            Image(systemName: leftBtnType.rawValue)
+            Spacer()
+            if isDisplayTitle {
+                Text(title)
+                    .font(.pretendardSemiBold_24)
+            }
+            Spacer()
+        }
+        .padding(20)
+        .padding(.bottom, 20)
+    }
+}
+
+#Preview {
+    SettingNavigationTitle()
+}
