589884faf988c207394fe629800ba89e672e38d9
TAEJUN <43903354+htj7425@users.noreply.github.com>
2024-02-05 17:41:07 +0900

    👀 [Feat] User Model 작성

---
+++ EYE-Mate/EYE-Mate/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  EYE-Mate
+//
+//  Created by Taejun Ha on 2/2/24.
+//
+
+import FirebaseFirestore
+
+struct User: Identifiable, Codable {
+    @DocumentID var id: String?
+    var username: String
+    var userUID: String
+    var userImageURL: URL
+    
+}
