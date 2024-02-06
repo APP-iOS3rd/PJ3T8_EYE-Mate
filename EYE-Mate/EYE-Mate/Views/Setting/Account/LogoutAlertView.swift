78331929c3f86872afc96f24b9df9b7cbf193f4d
Minyoung Lee <mminy62@gmail.com>
2024-02-06 16:13:51 +0900

    👀 [Feat] custom 로그아웃 알럿 뷰 추가

---
+++ EYE-Mate/EYE-Mate/Views/Setting/Account/LogoutAlertView.swift
@@ -0,0 +1,56 @@
+//
+//  CustomAlertButton.swift
+//  EYE-Mate
+//
+//  Created by 이민영 on 2024/02/05.
+//
+
+import SwiftUI
+
+struct LogoutAlertView: View {
+    @Binding var showAlert: Bool
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            Text("로그아웃")
+                .font(.pretendardMedium_22)
+                .padding(.bottom, 20)
+            
+            Text("로그아웃 하시겠습니까?")
+                .font(.pretendardMedium_18)
+                .foregroundColor(Color.gray)
+            
+            Spacer()
+            
+            HStack(spacing: 0) {
+                Button {
+                    showAlert = false
+                } label: {
+                    Text("취소")
+                        .foregroundColor(.black)
+                        .frame(width: UIScreen.main.bounds.width/2-30, height: 50)
+                        .background(Color.btnGray)
+                }
+                
+                Button {
+                    // MARK: - 로그아웃 처리
+                } label: {
+                    Text("확인")
+                        .foregroundColor(.black)
+                        .frame(width: UIScreen.main.bounds.width/2-30, height: 50)
+                        .background(Color.customGreen)
+                }
+                
+            }
+        }
+        .frame(width: UIScreen.main.bounds.width-60, height: 170)
+        .background(Color.white)
+        .cornerRadius(10)
+    }
+}
+
+#Preview {
+    LogoutAlertView(showAlert: .constant(false))
+}
