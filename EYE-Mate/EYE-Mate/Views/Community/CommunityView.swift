2f209fcfdf3e14a485dcb4fbe1f07d14dc663cf3
TAEJUN <43903354+htj7425@users.noreply.github.com>
2024-02-07 11:58:49 +0900

    ♻️ [Refactor] 게시판 View 코드 분리

--- EYE-Mate/EYE-Mate/Views/Community/CommunityView.swift
+++ EYE-Mate/EYE-Mate/Views/Community/CommunityView.swift
@@ -14,43 +14,41 @@ struct CommunityView: View {
     
     var body: some View {
         // MARK: NavigationStack 추후에 지워주기
-        NavigationStack{
-            VStack(spacing: 0) {
-                // 상단 Title
-                HStack(alignment: .bottom) {
-                    VStack {
-                        Text("EYE-Mate")
-                            .font(.pretendardBold_22)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        Text("게시판")
-                            .font(.pretendardBold_32)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                    }
-                    
-                    // MARK: profileImage 추후에 Firebase에서 Image 받아오기
-                    NavigationLink(destination: ProfileView()) {
-                        Image(systemName: "person.crop.circle.fill")
-                            .font(.largeTitle)
-                            .foregroundStyle(.black)
-                    }
-                }
-                .padding(.horizontal, 20)
-                .padding(.vertical)
-                
-                // 상단 TabView
-                communityTopTabView()
-                
-                // 선택된 상단 Tab의 View Switching
-                
-                switch selectedPicker {
-                case .faq:
-                    FAQView()
-                case .freeboard:
-                    FreeBoardView()
+        VStack(spacing: 0) {
+            // 상단 Title
+            HStack(alignment: .bottom) {
+                VStack {
+                    Text("EYE-Mate")
+                        .font(.pretendardBold_22)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Text("게시판")
+                        .font(.pretendardBold_32)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                 }
                 
-                Spacer()
+                // MARK: profileImage 추후에 Firebase에서 Image 받아오기
+                NavigationLink(destination: ProfileView()) {
+                    Image(systemName: "person.crop.circle.fill")
+                        .font(.largeTitle)
+                        .foregroundStyle(.black)
+                }
             }
+            .padding(.horizontal, 20)
+            .padding(.vertical)
+            
+            // 상단 TabView
+            communityTopTabView()
+            
+            // 선택된 상단 Tab의 View Switching
+            
+            switch selectedPicker {
+            case .faq:
+                FAQView()
+            case .freeboard:
+                FreeBoardView()
+            }
+            
+            Spacer()
         }
     }
     
