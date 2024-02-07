456cd896adf2edffb651c02ca3a89e81584c77df
S_HYEON <zxl3651@naver.com>
2024-02-07 19:26:09 +0900

    👀 [Feat] 눈운동 버튼 클릭시 탭 이동 기능 구현

--- EYE-Mate/EYE-Mate/Views/MainView.swift
+++ EYE-Mate/EYE-Mate/Views/MainView.swift
@@ -8,34 +8,36 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var index = 0
+    
     var body: some View {
         NavigationStack {
-            TabView {
-                HomeView()
+            TabView(selection: $index) {
+                HomeView(index: $index)
                     .tabItem {
                         Image(systemName: "house")
                         Text("홈")
                             .font(.pretendardMedium_10)
-                    }
+                    }.tag(0)
                 MovementView()
                     .tabItem {
                         Image(systemName: "eyes")
                         Text("눈운동")
                             .font(.pretendardMedium_10)
-                    }
+                    }.tag(1)
                 CommunityView()
                     .tabItem {
                         Image(systemName: "message.fill")
                         Text("게시판")
                             .font(.pretendardMedium_10)
-                    }
+                    }.tag(2)
                 EyeMapView()
                     .tabItem {
                         Image(systemName: "map.fill")
                             .imageScale(.small)
                         Text("내주변")
                             .font(.pretendardMedium_10)
-                    }
+                    }.tag(3)
             }
             .accentColor(.customGreen)
             .padding(0)
