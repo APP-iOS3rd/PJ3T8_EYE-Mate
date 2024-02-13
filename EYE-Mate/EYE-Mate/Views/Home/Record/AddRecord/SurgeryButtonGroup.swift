5bdfefa3149e7ea21639796af0b6f3e74537238b
seongjun.oh <tiger8708@gmail.com>
2024-02-13 15:35:02 +0900

    👀 [Feat] WrappingHStack 구현

---
+++ EYE-Mate/EYE-Mate/Views/Home/Record/AddRecord/SurgeryButtonGroup.swift
@@ -0,0 +1,18 @@
+//
+//  SurgeryButtonGroup.swift
+//  EYE-Mate
+//
+//  Created by seongjun on 2/13/24.
+//
+
+import SwiftUI
+
+struct SurgeryButtonGroup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SurgeryButtonGroup()
+}
