2f0b2d39f979b93803710f439fe805fbc836aa91
TAEJUN <43903354+htj7425@users.noreply.github.com>
2024-02-16 19:01:29 +0900

    👀 [Feat] PostViewModel 댓글, 대댓글 작성 및 삭제 추가

--- EYE-Mate/EYE-Mate/Views/Community/FreeBoard/Post/PostViewModel.swift
+++ EYE-Mate/EYE-Mate/Views/Community/FreeBoard/Post/PostViewModel.swift
@@ -13,18 +13,27 @@ import FirebaseStorage
 class PostViewModel: ObservableObject {
     @Published var post: Post
     
-    @Published var commentText: String = ""
-    
     @Published var isLoading: Bool = false
     
+    @Published var commentText: String = ""
+    @Published var commentPlaceholder: String = "댓글을 입력해보세요..."
     @Published var commentViewPadding: CGFloat = 15
     @Published var commentViewCornerRadius: CGFloat = 10
     
+    @Published var newComment: Comment? // 새롭게 작성된 댓글
+    @Published var newReplyComment: ReplyComment? // 새롭게 작성된 대댓글
+    
+    @Published var replyWritingCommentID: String? // 대댓글 작성중인 댓글의 댓글 ID
+    @Published var replyWritingCommentIndex: Int? // 대댓글 작성중인 댓글의 댓글 Index
+    
     @State private var docListener: ListenerRegistration?
+    @State private var paginationDoc: QueryDocumentSnapshot?
     
     @AppStorage("user_name") private var userName: String = ""
     @AppStorage("user_UID") var userUID: String = ""
     @AppStorage("user_profile_url") private var profileURL: URL?
+    
+    private let dbRef = Firestore.firestore().collection("Posts")
     
     init(post: Post) {
         self.post = post
@@ -34,19 +43,16 @@ class PostViewModel: ObservableObject {
     func addDocListener(completion: @escaping (Post?) -> ()) {
         if docListener == nil {
             guard let postID = post.id else { return }
-            docListener = Firestore.firestore().collection("Posts").document(postID).addSnapshotListener({ snapshot, error in
-                if let snapshot {
-                    if snapshot.exists {
-                        if let updatedPost = try? snapshot.data(as: Post.self) {
-                            // 변경 사항 있으면 수정
-                            completion(updatedPost)
-                        }
-                    } else {
-                        // 게시물 없으면 삭제
-                        completion(nil)
+            docListener = dbRef.document(postID).addSnapshotListener { snapshot, error in
+                if let snapshot = snapshot, snapshot.exists {
+                    if let updatedPost = try? snapshot.data(as: Post.self) {
+                        completion(updatedPost)
                     }
+                } else {
+                    // 게시물이 삭제된 경우
+                    completion(nil)
                 }
-            })
+            }
         }
     }
     
@@ -56,14 +62,22 @@ class PostViewModel: ObservableObject {
             guard let postID = post.id else { return }
             if post.likedIDs.contains(userUID) {
                 // 배열에서 사용자 ID 제거
-                try await Firestore.firestore().collection("Posts").document(postID).updateData([
+                try await dbRef.document(postID).updateData([
                     "likedIDs": FieldValue.arrayRemove([userUID])
                 ])
             } else {
-                try await Firestore.firestore().collection("Posts").document(postID).updateData([
+                try await dbRef.document(postID).updateData([
                     "likedIDs": FieldValue.arrayUnion([userUID])
                 ])
             }
+        }
+    }
+    
+    /// - docListener 제거
+    func removeDocListener() {
+        if let docListener {
+            docListener.remove()
+            self.docListener = nil
         }
     }
     
@@ -81,7 +95,20 @@ class PostViewModel: ObservableObject {
                 
                 // 게시물 삭제
                 guard let postID = post.id else { return }
-                try await Firestore.firestore().collection("Posts").document(postID).delete()
+                try await dbRef.document(postID).delete()
+                
+                // 댓글 삭제
+                let commentsQuerySnapshot = try await dbRef.document(postID).collection("Comments").getDocuments()
+                
+                for document in commentsQuerySnapshot.documents {
+                    try await document.reference.delete()
+                    
+                    let replyCommentsQuerySnapshot = try await document.reference.collection("ReplyComments").getDocuments()
+                    
+                    for replyDocument in replyCommentsQuerySnapshot.documents {
+                        try await replyDocument.reference.delete()
+                    }
+                }
             } catch {
                 print(error.localizedDescription)
             }
@@ -89,19 +116,74 @@ class PostViewModel: ObservableObject {
     }
     
     /// - 댓글 작성 Action
-    func writeComment() {
+    func writeComment(completion: @escaping () -> ()) {
+        isLoading = true
         let comment = Comment(userName: userName,
                               userUID: userUID,
                               userImageURL: profileURL,
                               comment: commentText)
+                
         Task {
             do {
                 guard let postID = post.id else { return }
-//                try await Firestore.firestore().collection("Posts").document(postID).collection()
+                
+                let doc = dbRef.document(postID).collection("Comments").document()
+                
+                try doc.setData(from: comment)
+                
+                await MainActor.run {
+                    var commentData = comment
+                    commentData.id = doc.documentID
+                    
+                    commentText = ""
+                    post.comments.append(commentData)
+                    newComment = commentData
+                    isLoading = false
+                    completion()
+                }
             } catch {
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    /// - 대댓글 작성 Action
+    func writeReplyComment(completion: @escaping (String, Int, ReplyComment) -> ()) { // postID, commentIndex, [ReplyComment]
+        isLoading = true
+        let replyComment = ReplyComment(userName: userName,
+                                        userUID: userUID,
+                                        userImageURL: profileURL,
+                                        comment: commentText)
+        
+        Task {
+            do {
+                guard let postID = post.id, let commentID = replyWritingCommentID, let commentIndex = replyWritingCommentIndex else { return }
+                
+                let doc = dbRef.document(postID).collection("Comments").document(commentID).collection("ReplyComments").document()
+                
+                try doc.setData(from: replyComment)
+                
+                await MainActor.run {
+                    var replyCommentData = replyComment
+                    replyCommentData.id = doc.documentID
+                    
+                    commentText = ""
+                    post.comments[commentIndex].replyComments.append(replyCommentData)
+                    newReplyComment = replyCommentData
+                    self.commentPlaceholder = "댓글을 입력해보세요..."
+                    
+                    isLoading = false
+                    completion(postID, commentIndex, replyCommentData)
+                }
+            }
+        }
+    }
+    
+    /// - 대댓글 작성 시작 Action
+    func startWritingReplyComment(commentID: String, commentIndex: Int) {
+        self.replyWritingCommentID = commentID
+        self.replyWritingCommentIndex = commentIndex
+        self.commentPlaceholder = "대댓글을 입력해보세요..."
     }
     
     /// - 게시물 스크랩 Action
