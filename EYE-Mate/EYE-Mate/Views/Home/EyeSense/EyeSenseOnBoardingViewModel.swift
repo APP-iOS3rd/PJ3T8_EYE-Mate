9c7285a360a837236ea18f655f2640e7071f8ff9
Minyoung Lee <mminy62@gmail.com>
2024-02-20 10:30:32 +0900

    👀 [Feat] firestore에서 랜덤으로 기사 데이터 가져오기

--- EYE-Mate/EYE-Mate/Views/Home/EyeSense/EyeSenseOnBoardingViewModel.swift
+++ EYE-Mate/EYE-Mate/Views/Home/EyeSense/EyeSenseOnBoardingViewModel.swift
@@ -6,13 +6,50 @@
 //
 
 import Foundation
+import FirebaseFirestore
 
-struct EyeSenseOnBoardingViewModel {
+struct Article {
     var title: String
-    var subTitle: String
+    var url: String
+}
+
+class EyeSenseOnBoardingViewModel: ObservableObject {
+    @Published var articles: [Article] = []
+    var title: String
     
-    init(title: String, subTitle: String) {
+    let db = Firestore.firestore()
+    
+    init(title: String = "") {
         self.title = title
-        self.subTitle = subTitle
+    }
+}
+
+extension EyeSenseOnBoardingViewModel {
+    
+    @MainActor
+    func fetchData() async {
+        do {
+            let query = db.collection("Articles")
+            let snapshot = try await query.getDocuments()
+            let totalCount = snapshot.count
+            let randomIndices = (0..<totalCount).shuffled().prefix(3)
+            var selectedDocuments: [DocumentSnapshot] = []
+            
+            for index in randomIndices {
+                let document = snapshot.documents[index]
+                selectedDocuments.append(document)
+            }
+            
+            for document in selectedDocuments {
+                print("\(document.documentID) => \(document.data()!)")
+                
+                // articles에 정보 담기
+                if let data = document.data() {
+                    articles.append(Article(title: data["title"] as! String, url: data["url"] as! String))
+                }
+            }
+        } catch {
+            print("Error getting documents: \(error)")
+        }
     }
 }
