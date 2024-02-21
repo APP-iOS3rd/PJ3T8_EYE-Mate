89ffa5a1268aeb418470a8cc6df1c7227618c3d9
TAEJUN <43903354+htj7425@users.noreply.github.com>
2024-02-21 16:01:15 +0900

    👀 [Feat] 게시물 이미지 크게보기 및 닫기, zoom 제스쳐 추가

---
+++ EYE-Mate/EYE-Mate/Views/Community/FreeBoard/Post/ExpandImageView.swift
@@ -0,0 +1,78 @@
+//
+//  ExpandImageView.swift
+//  EYE-Mate
+//
+//  Created by Taejun Ha on 2/21/24.
+//
+
+import SwiftUI
+
+struct ExpandImageView: View {
+    @ObservedObject var postVM: PostViewModel
+    @GestureState var draggingOffset: CGSize = .zero
+    
+    var body: some View {
+        ZStack {
+            Color.bg
+                .opacity(postVM.bgOpacity)
+                .ignoresSafeArea()
+            
+            TabView(selection: $postVM.selectedImageIndex){
+                ForEach(postVM.selectedImages.indices, id: \.self) { index in
+                    postVM.selectedImages[index]
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .tag(index)
+                        .scaleEffect(postVM.selectedImageIndex == index ? (postVM.imageScale > 1 ? postVM.imageScale : 1) : 1)
+                        .offset(y: postVM.imageViewerOffset.height)
+                        // 확대 제스쳐 Zoom
+                        .gesture(
+                            MagnificationGesture().onChanged({ value in
+                                postVM.imageScale = value
+                            }).onEnded({ _ in
+                                withAnimation(.spring) {
+                                    postVM.imageScale = 1
+                                }
+                            })
+                            // Double to Zoom
+                                .simultaneously(with: TapGesture(count: 2).onEnded({
+                                    withAnimation {
+                                        postVM.imageScale = postVM.imageScale > 1 ? 1 : 4
+                                    }
+                                }))
+                        )
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+            .overlay(alignment: .topTrailing) {
+                // Close Button
+                Button {
+                    withAnimation(.default) {
+//                        postVM.selectedImageIndex = -1
+                        postVM.showImageViewer.toggle()
+                    }
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundStyle(.white)
+                        .padding()
+                        .background(Color.white.opacity(0.35))
+                        .clipShape(Circle())
+                }
+                .padding(10)
+                .opacity(postVM.bgOpacity)
+            }
+        }
+        .gesture(DragGesture().updating($draggingOffset, body: { value, outValue, _ in
+            outValue = value.translation
+            postVM.onChangeImageViewer(value: draggingOffset)
+        }).onEnded(postVM.onEnd(value:)))
+    }
+}
+
+extension Color {
+    static let bg = Color.black.opacity(0.95)
+}
+
+//#Preview {
+//    ExpandImageView()
+//}
--- EYE-Mate/EYE-Mate/Views/Community/FreeBoard/Post/ImageCardView.swift
+++ EYE-Mate/EYE-Mate/Views/Community/FreeBoard/Post/ImageCardView.swift
@@ -6,34 +6,37 @@
 //
 
 import SwiftUI
+
 import Kingfisher
 
 struct ImageCardView: View {
     var imageURLs: [URL]
+    @ObservedObject var postVM: PostViewModel
     
     var body: some View {
         ScrollView(.horizontal) {
             HStack(spacing: 0) {
-                ForEach(imageURLs, id: \.self) {
-                    KFImage($0)
-                        .placeholder {
-                            ProgressView()
-                                .modifier(MapImageModifier())
-                        }
-                        .retry(maxCount: 3, interval: .seconds(5))
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 150, height: 150)
-                        .background(.black)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .padding(.trailing, 5)
+                ForEach(imageURLs.indices, id: \.self) { index in
+                    Button {
+                        postVM.selectedImageIndex = index
+                        postVM.showImageViewer.toggle()
+                    } label: {
+                        KFImage(imageURLs[index])
+                            .placeholder {
+                                ProgressView()
+                                    .modifier(MapImageModifier())
+                            }
+                            .retry(maxCount: 3, interval: .seconds(5))
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 150, height: 150)
+                            .background(.black)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                    .padding(.trailing, 5)
                 }
             }
         }
         .scrollIndicators(.never)
     }
 }
-
-//#Preview {
-//    ImageCardView(imageURLs: FreeBoardViewModel().posts[0].postImageURLs)
-//}
