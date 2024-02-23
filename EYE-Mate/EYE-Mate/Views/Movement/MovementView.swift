636fed9d861de3945a771de93e781a6956631bf1
seongjun.oh <tiger8708@gmail.com>
2024-02-23 17:23:26 +0900

    💄 [Design] 눈 운동 화면 디자인 변경

--- EYE-Mate/EYE-Mate/Views/Movement/MovementView.swift
+++ EYE-Mate/EYE-Mate/Views/Movement/MovementView.swift
@@ -14,6 +14,8 @@ extension View {
 }
 
 struct MovementView: View {
+    @AppStorage("user_name") private var userName: String = "EYE-Mate"
+
     @ObservedObject private var profileViewModel = ProfileViewModel.shared
     @ObservedObject private var movementViewModel = MovementViewModel.shared
 
@@ -24,44 +26,39 @@ struct MovementView: View {
     var body: some View {
         VStack(spacing: 0) {
             HorizontalDivider(color: Color.customGreen, height: 4)
-            VStack(alignment: .leading, spacing: 16) {
-                VStack(alignment: .leading) {
-                    Text("어디로 가야 하오 님!")
-                        .font(.pretendardSemiBold_22)
-                    Text("오늘도 눈 건강 챙기셨나요? 👀")
-                        .font(.pretendardRegular_22)
+            ScrollView {
+                VStack(alignment: .leading, spacing: 16) {
+                    Spacer().frame(height: 12)
+                    VStack(alignment: .leading) {
+                        Text("\(userName) 님!")
+                            .font(.pretendardSemiBold_22)
+                        Text("오늘도 눈 건강 챙기셨나요? 👀")
+                            .font(.pretendardRegular_22)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    Spacer().frame(height: 24)
+                    ForEach(movementList, id: \.self) { movement in
+                        StartMovementRow(movementType: movement)
+                    }
+                    .padding(.horizontal, -10)
+                    .padding(.vertical, 0)
+                    .listStyle(PlainListStyle())
+                    .scrollDisabled(true)
+                    .scrollContentBackground(.hidden)
+                    Spacer()
+                    VStack(alignment: .leading) {
+                        Text("추후 다른 운동 업데이트 예정입니다.")
+                            .font(.pretendardMedium_18)
+                            .foregroundColor(Color.warningGray)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    Spacer()
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                VStack(alignment: .leading) {
-                    Text("#오늘의 눈 운동")
-                        .font(.pretendardRegular_16)
-                    Text("0회")
-                        .font(.pretendardSemiBold_20)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                ForEach(movementList, id: \.self) { movement in
-                    StartMovementRow(movementType: movement)
-                }
-                .padding(.horizontal, -10)
-                .padding(.vertical, 0)
-                .listStyle(PlainListStyle())
-                .scrollDisabled(true)
-                .scrollContentBackground(.hidden)
-                Spacer()
-                VStack(alignment: .leading) {
-                    Text("추후 다른 운동 업데이트 예정입니다.")
-                        .font(.pretendardMedium_18)
-                        .foregroundColor(Color.warningGray)
-                }
-                .frame(maxWidth: .infinity, alignment: .center)
-                Spacer()
+                .padding(.horizontal, 32)
+                .padding(.top, 16)
             }
-            .padding(.horizontal, 32)
-            .padding(.top, 16)
-            .background(Color.textFieldGray)
-            Spacer()
-                .frame(height: 70)
         }
+        .background(Color.textFieldGray)
         .toastView(toast: $toast)
         .onAppear {
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
