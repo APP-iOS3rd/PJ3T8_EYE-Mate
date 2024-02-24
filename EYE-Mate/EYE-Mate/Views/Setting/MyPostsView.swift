c1053752c51a0ab9a650e9b75e708ea5bb1c25b0
TAEJUN <43903354+htj7425@users.noreply.github.com>
2024-02-25 01:09:56 +0900

    👀 [Feat] 내가 쓴 게시물 Navigation Title 추가

--- EYE-Mate/EYE-Mate/Views/Setting/MyPostsView.swift
+++ EYE-Mate/EYE-Mate/Views/Setting/MyPostsView.swift
@@ -9,11 +9,31 @@ import SwiftUI
 
 struct MyPostsView: View {
     @StateObject var freeboardViewModel: FreeBoardViewModel = FreeBoardViewModel()
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        ZStack {
-            ReusablePostsView(freeboardVM: freeboardViewModel, fetchCase: .myPost)
-        }
+        ReusablePostsView(freeboardVM: freeboardViewModel, fetchCase: .myPost)
+            .navigationBarBackButtonHidden()
+            .toolbar(content: {
+                // MARK: Back Button
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        /// 현재 화면 dismiss
+                        dismiss()
+                    } label: {
+                        Image(systemName: "chevron.backward")
+                            .foregroundStyle(.black)
+                    }
+                }
+                
+                // MARK: Navigation Title
+                ToolbarItem(placement: .principal) {
+                    HStack {
+                        Text("작성한 게시글")
+                            .font(.pretendardSemiBold_24)
+                    }
+                }
+            })
     }
 }
 
