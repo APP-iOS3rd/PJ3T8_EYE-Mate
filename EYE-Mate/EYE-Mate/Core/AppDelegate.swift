20899ab7248507d03262812e0ee4047fac77c85a
S_HYEON <zxl3651@naver.com>
2024-02-25 19:21:59 +0900

    👀 [Feat] localpush 구현

---
+++ EYE-Mate/EYE-Mate/Core/AppDelegate.swift
@@ -0,0 +1,67 @@
+//
+//  AppDelegate.swift
+//  EYE-Mate
+//
+//  Created by 이성현 on 2024/02/23.
+//
+
+import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+import UserNotifications
+
+// firebase 연결함수
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    let notificationManager = NotificationManager.instance
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        UNUserNotificationCenter.current().delegate = self
+        notificationManager.removeAllPendingNotifications()
+        
+        // 권한 요청
+        notificationManager.requestAuthorization { granted in
+            if granted {
+                self.notificationManager.scheduleNotification()
+            }
+        }
+        
+        return true
+    }
+    
+    // 토큰 받아오는 함수
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Auth.auth().setAPNSToken(deviceToken, type: .sandbox)
+    }
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // 사용자의 데이터 보내기
+        // 우선 nodata로 아무것도 안보냄
+        if Auth.auth().canHandleNotification(userInfo) {
+            completionHandler(.noData)
+        }
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if Auth.auth().canHandle(url){
+            return true
+        }
+        return false
+    }
+
+    static var orientationLock = UIInterfaceOrientationMask.portrait
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // 알림이 도착했을 때 호출
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+        notificationManager.updateBadge()
+    }
+}
--- EYE-Mate/EYE-Mate/Core/EYE_MateApp.swift
+++ EYE-Mate/EYE-Mate/Core/EYE_MateApp.swift
@@ -11,7 +11,6 @@ import FirebaseAuth
 
 @main
 struct EYE_MateApp: App {
-
     // register app delegate for Firebase setup
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @ObservedObject var router = Router()
@@ -60,40 +59,5 @@ struct EYE_MateApp: App {
             }
             .environmentObject(router)
         }
-    }
-}
-
-// firebase 연결함수
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-
-        return true
-    }
-    // 토큰 받아오는 함수
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        Auth.auth().setAPNSToken(deviceToken, type: .sandbox)
-    }
-
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        // 사용자의 데이터 보내기
-        // 우선 nodata로 아무것도 안보냄
-        if Auth.auth().canHandleNotification(userInfo) {
-            completionHandler(.noData)
-        }
-    }
-
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        if Auth.auth().canHandle(url){
-            return true
-        }
-        return false
-    }
-
-    static var orientationLock = UIInterfaceOrientationMask.portrait
-
-    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-        return AppDelegate.orientationLock
     }
 }
